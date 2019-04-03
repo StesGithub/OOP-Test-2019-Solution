d27a7576f0b483fb2d9b5972214981206e369498
skooter500 <skooter500@gmail.com>
2019-04-03 16:32:26 +0100

    Empty project

---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+public class Main
+{	
+
+	public void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Resistance());
+	}
+	
+	
+
+	public static void main(String[] args)
+	{
+
+		Main main = new Main();
+		main.helloProcessing();		
+	}
+}
---
+++ src/ie/tudublin/Resistance.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Resistance extends PApplet
+{	
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+	public void setup() 
+	{
+	}
+	
+	public void draw()
+	{			
+		background(0);
+	}
+}
